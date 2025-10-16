16ab2dd52cabdce5cfb69166d5b67bc1396d932e
m.rizki akbar <m.rizki.x31@gmail.com>
2025-10-16 20:32:54 +0700

    gg

---
+++ tes_push/fbhk.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam ab sed odit officia saepe. Molestias similique possimus dignissimos nemo provident delectus aliquid ratione obcaecati nostrum est, officiis eius, exercitationem quae.
+</body>
+</html>
