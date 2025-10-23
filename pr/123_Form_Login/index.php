ab675753e70d20a9928b102fd23760a6317c775f
Aldhi <aldhi.albadri@gmail.com>
2025-10-23 20:47:53 +0700

    selesai 123_form_login

---
+++ pr/123_Form_Login/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="#" method="POST">
+        <div>
+            <label for="">Username</label>
+            <input type="text">
+        </div>
+        <div>
+            <label for="">Password</label>
+            <input type="password">
+        </div>
+        <div>
+            <button type="submit">Login</button>
+        </div>
+    </form>
+</body>
+
+</html>
