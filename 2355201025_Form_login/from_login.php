3641640b5d43f97a49dc27377d87ba069ea443a5
m.rizki akbar <m.rizki.x31@gmail.com>
2025-10-24 18:51:59 +0700

    example

--- 2355201025_Form_login/from_login.php
+++ 2355201025_Form_login/from_login.php
@@ -1,1 +1,15 @@
-Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus quisquam quo provident ipsam tempore officiis a ex dolor doloremque hic odit velit mollitia aliquam, consectetur placeat sit corrupti earum nobis.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <label for="">masukan username:</label>
+    <input type="text">
+    <label for="">masukan password:</label>
+    <input type="password">
+    <button>submit</button>
+</body>
+</html>
