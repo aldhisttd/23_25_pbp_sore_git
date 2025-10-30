a65755eb65661720a14cd3986ebcfd10bc20205b
rivaldo <rahmatrivaldo666@gmail.com>
2025-10-30 15:19:03 +0700

    add form login di branch 2355201023

---
+++ pr/login_form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+</head>
+<body>
+
+<form action="#" method="POST">
+    <div>
+        <label for="username">Username</label>
+        <input type="password" name="password" id="">
+        <br>
+    </div>
+    <br>
+    <button type="submit">Login</button>
+</form>
+    
+</body>
+</html>ml
