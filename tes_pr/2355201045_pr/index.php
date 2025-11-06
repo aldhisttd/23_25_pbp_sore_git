6f6d77d5a6e5a071b5a5be1a135c85c1e3e26330
ramadhan <brigademobile04@gmail.com>
2025-11-06 20:44:54 +0700

    final

--- tes_pr/2355201045_pr/index.php
+++ tes_pr/2355201045_pr/index.php
@@ -1,4 +1,11 @@
-<html>
-
-    <h1 style="color : red">Muhammad Hafiz Ramadhan</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 style="color: red;">Muhammad Hafiz Ramadhan</h1>
+</body>
 </html>
