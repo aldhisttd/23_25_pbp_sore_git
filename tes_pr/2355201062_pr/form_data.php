a2601495621dadb03cb3efec77af8cf580f63e57
desi <desikhairunnisa947@gmail.com>
2025-11-23 20:03:21 +0700

    upgrate

---
+++ tes_pr/2355201062_pr/form_data.php
@@ -0,0 +1,81 @@
+<?php
+// ini code untuk proses request yang formatnya formdata
+header("Content-Type: application/json; charset=UTF-8");
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    http_response_code(405);
+    $res = [
+        'status' => 'error',
+        'msg' => 'Method salah !'
+    ];
+    echo json_encode($res);
+    exit();
+}
+
+$json = file_get_contents("php://input");
+$data = json_decode($json, true);
+
+
+// validasi payload
+$errors = [];
+if (!isset($data['nim'])) {
+    $errors['nim'] = "NIM belum dikirim";
+} else {
+    if ($data['nim'] == '') {
+        $errors['nim'] = "NIM tidak boleh kosong";
+    } else {
+        if (!preg_match('/^[1-9][0-9]{9}$/', $data['nim'])) {
+            $errors['nim'] = "Format NIM harus angka 10 digit, angka awal tidak boleh 0";
+        }
+    }
+}
+
+if (!isset($data['nama'])) {
+    $errors['nama'] = "NAMA belum dikirim";
+} else {
+    if ($data['nama'] == '') {
+        $errors['nama'] = "NAMA tidak boleh kosong";
+    }
+}
+
+if (count($errors) > 0) {
+    http_response_code(400);
+    $res = [
+        'status' => 'error',
+        'msg' => "Error data",
+        'errors' => $errors
+    ];
+
+    echo json_encode($res);
+    exit();
+}
+
+// insert ke db
+$koneksi = new mysqli('localhost', 'root', '', 'be');
+$nim = $data['nim'];
+$nama = $data['nama'];
+$q = "INSERT INTO mahasiswa(nim, nama) VALUES('$nim','$nama')";
+$koneksi->query($q);
+$id = $koneksi->insert_id;
+
+echo json_encode([
+    'status' => 'success',
+    'msg' => 'Proses berhasil',
+    'data' => [
+        'id' => $id,
+        'nim' => $nim,
+        'nama' => $nama
+    ]
+]);
+
+header("Content-Type: application/json; charset=UTF-8");
+http_response_code(500);
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    http_response_code(405);
+    $res = [
+        'status' => 'error',
+        'msg' => 'Method salah !'
+    ];
+    echo json_encode($res);
+    exit();
+}
